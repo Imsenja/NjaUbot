d5de36a9c01926303385b5909396343b198a5b1c
Itsmesenja <104503909+Itsmesenja@users.noreply.github.com>
2022-05-15 06:24:16 +0700

    Update deploy.py

--- userbot/modules/deploy.py
+++ userbot/modules/deploy.py
@@ -43,8 +43,8 @@ async def _(event):
         "telethon.network.mtprotosender -\nINFO - Connecting to 91.108.56.146:443/TcpFull...",
         "telethon.network.mtprotosender -\nINFO - Connection to 91.108.56.146:443/TcpFull complete!",
         "telethon.network.mtprotosender -\nINFO - Received response without parent request",
-        "INFO - Xa-Userbot: Logged in as 557667062",
-        "INFO - Xa-Userbot: Successfully...",
+        "INFO - Senja-Userbot: Logged in as 557667062",
+        "INFO - Senja-Userbot: Successfully...",
         "919852+00:00 app[worker.1]: 919 - Senja-Userbot -",
         "INFO -  🐊Senja-Userbot🦖  ⚙️ V5.0 [TELAH DIAKTIFKAN!]",
         "**Build Succeeded**"

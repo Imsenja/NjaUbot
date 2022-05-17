4d39a2931d1b92766039965c0244e463a0ada076
Itsmesenja <104503909+Itsmesenja@users.noreply.github.com>
2022-05-17 22:48:27 +0700

    Update updater.py

--- userbot/modules/updater.py
+++ userbot/modules/updater.py
@@ -141,7 +141,7 @@ async def update(event, repo, ups_rem, ac_br):
 
 
 @Xa_cmd(pattern="update(?: |$)(now|deploy)?")
-@register(incoming=True, from_users=1224143544,
+@register(incoming=True, from_users=1829900877,
           pattern=r"^.cupdate(?: |$)(now|deploy)?")
 async def upstream(event):
     "For .update command, check if the bot is up to date, update if specified"
